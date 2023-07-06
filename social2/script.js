b65dc65dc5377d9237bd3e040b80465ea825493f
fadi kbb <fadikbb2004t@gmail.com>
2023-07-06 16:27:20 +0300

    destsre

--- social2/script.js
+++ social2/script.js
@@ -1,12 +1,11 @@
-const share = document.getElementsByClassName("share")
-
+const share = document.querySelector(".share")
+const like = document.querySelector(".like")
 share.addEventListener('click', event => {
-
     if (navigator.share) {
-
         navigator.share({
-            Text: 'please read this great article:',
-            url: 'https://www.google.com/'
+            text: `welcome to my profile:
+`,
+            url: 'https://fadikbb.netlify.app/',
 
         }).then(() => {
             console.log('thanks for sharing!');
@@ -16,3 +15,37 @@ share.addEventListener('click', event => {
         alert("the current browser does not support the sjare function. please ,manually share the link")
     }
 });
+let textShare = document.querySelector('.text-share');
+share.addEventListener('click', e => {
+    textShare.innerHTML++
+})
+
+
+
+var storedItem = localStorage.getItem("storedItem");
+
+function save(){
+    var Item = document.querySelector(".text-like").innerHTML;
+    localStorage.setItem("storedItem", Item);
+    document.querySelector(".text-like").innerHTML = Item;
+}
+
+function get(){
+    localStorage.getItem("storedItem");
+    document.querySelector(".text-like").innerHTML = storedItem;
+
+}
+
+const likeIcon = document.querySelector(".fa-heart")
+let textLike = document.querySelector('.text-like');
+like.addEventListener('click', e => {
+    if(likeIcon.style.color == "red") {
+        likeIcon.style.color = "black";
+        textLike.innerHTML++;
+    } 
+    else {
+        likeIcon.style.color = "red";
+        textLike.innerHTML--;
+    }
+
+})
