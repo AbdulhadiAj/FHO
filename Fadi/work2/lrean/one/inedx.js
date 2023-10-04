745aad0d2f7b4433a506d6bae53bd3219976759f
fadi kbb <fadikbb2004t@gmail.com>
2023-10-04 18:14:54 +0300

    sdfawefagb

---
+++ Fadi/work2/lrean/one/inedx.js
@@ -0,0 +1,91 @@
+// //log for print in console
+// console.log(
+//   "%cgsdfgseeas fa asera %cew rgeg",
+//   "color: red; font-size: 40px",
+//   "color: blue; font-size: 40px"
+// );
+// //to see error in console
+// console.error("error");
+// //-------------------------------------------------------------------------
+// console.table(["ahmad", "masf", "sadfas"]);
+// my_name = "osama";
+// window.alert(`hello ${my_name}`);
+// document.write(`hello ${my_name}` + " ");
+// console.log(`hello %c${my_name}`, "color:red;font-size:30px;");
+// window.alert("hello " + my_name);
+// document.write(" hello " + my_name);
+// console.log(" hello  " + "%c" + my_name, "color:red;font-size:30px;");
+// //--------------------------------------------------------------------------
+// //data type
+// console.log(typeof "FADI");
+// console.log(typeof 50);
+// console.log(typeof 55.5);
+// console.log(typeof [50, 5, 12]);
+// console.log(typeof ["fadi", "osama", "samir"]);
+// console.log(typeof { name: "fadi", age: 50, country: "lb" });
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// //--------------------------------------------------------------------------
+// // make variable
+// var user = "osama";
+// console.log(user);
+// console.log(hello);
+// //innerHTML for edit in HTMl
+// hello.innerHTML = "option";
+//---------------------------------------------------------------------------
+//challange
+// let title = "elzero ",
+//   paragraph = "elzero web school",
+//   date = "10/15",workup=`<div class="all">
+//   <h1 class="title">hello ${title}</h1>
+//   <p>${paragraph}</p>
+//   <div class="time">${date}</div>
+//   </div>`;
+
+// document.write(workup.repeat(4));
+//----------------------------------------------------------------------------
+// //arithmetic operator
+// console.log(10 + 10);//addition
+// console.log(10 + " osama");//addition with two string
+//console.log(+"10"+10)//retrun number
+// console.log(10 - 10);//subtraction
+// console.log(10 - " osama"); //nan
+// console.log(typeof NaN);
+// console.log(10 * 20);//multiplication
+// console.log(10 * (-20 * 8));//multiplication
+// console.log(2 ** 4);//power
+// console.log(10 % 2);//modelo
+// console.log(11 % 2);//modelo
+// let num = 1;
+// num++;//increment
+// ++num;//increment
+// num--;//decrement
+// --num;//decrement
+// ++num;//increment
+//--------------------------------------------------------------------------------
+// assignment operators
+// let a = 10;
+// a = a + 20;
+// //or
+// a += 20;
+// //and
+// a = a - 20;
+// //or
+// a -= 20;
+// //and
+// a = a / 20;
+// //or
+// a /= 20;
+// //and
+// a = a % 20;
+// //or
+// a %= 20;
+//-----------------------------------------------------------------------------------
+let a = "-100";
+let b = "20";
+let c = 30;
+let d = true;
+console.log(-a * b); //2000
+console.log(-(--a) * +(++d) - --c); //173
