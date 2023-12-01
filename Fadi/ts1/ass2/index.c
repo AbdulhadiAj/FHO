4d81e5bdff504b87c23fd56f6e11b836e1a6ff33
unknown <fadikbb2004t@gmail.com>
2023-12-01 20:01:54 +0200

    new file

--- Fadi/ts1/ass2/index.c
+++ Fadi/ts1/ass2/index.c
@@ -33,19 +33,19 @@ int main()
     // {
     //     printf("freezing weather");
     // }
-    // else if (temp >= 0 && temp < 10)
+    // else if ( temp < 10)
     // {
     //     printf("very cold weather");
     // }
-    // else if (temp >= 10 && temp < 20)
+    // else if (temp < 20)
     // {
     //     printf("cold weather");
     // }
-    // else if (temp >= 20 && temp < 30)
+    // else if (temp < 30)
     // {
     //     printf("normal in temp");
     // }F
-    // else if (temp >= 30 && temp < 40)
+    // else if (temp < 40)
     // {
     //     printf("it's hot");
     // }
